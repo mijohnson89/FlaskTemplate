d3169249bfce345df2325c900b8b2dbf53447f32
mijohnson89 <mijohnson89@users.noreply.github.com>
2023-04-20 10:23:37 +0000

    ✅ Ready to clone and code.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,9 @@
 import sys
 import pytest
 
-from project_name import create_app
-from project_name.ext.commands import populate_db
-from project_name.ext.database import db
+from flasktemplate import create_app
+from flasktemplate.ext.commands import populate_db
+from flasktemplate.ext.database import db
 
 
 @pytest.fixture(scope="session")
